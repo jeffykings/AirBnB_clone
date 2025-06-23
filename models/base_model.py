48a313fa82fd59343861759c6e2e1fc6fad84bf6
jeffykings <jeffykings@gmail.com>
2025-06-23 20:30:23 +0100

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,19 @@
 import uuid
 from datetime import datetime
 """
-Contains the model that other classes inherites
+Contains the model that other classes inherits
 """
 
 
 class BaseModel():
-
     """
     Defines all common attributes/methods for other classes:
     """
 
     def __init__(self):
+        """
+        It is first thing to run a class is called
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
@@ -26,7 +28,7 @@ class BaseModel():
         changene the public instance attribute updated_at with the current
         datetime = self.__dict__[created_at]
         """
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -36,4 +38,5 @@ class BaseModel():
         new_dict = dict(self.__dict__)
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
