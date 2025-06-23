807f28e0d62117357b1175929059ab02b0145ddb
jeffykings <jeffykings@gmail.com>
2025-06-23 19:53:28 +0100

    task3

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/pyhton3
+
+import uuid
+from datetime import datetime
+"""
+Contains the model that other classes inherites
+"""
+
+
+class BaseModel():
+
+    """
+    Defines all common attributes/methods for other classes:
+    """
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        changene the public instance attribute updated_at with the current
+        datetime = self.__dict__[created_at]
+        """
+        self.updated_at = datetime.now().isoformat()
+
+    def to_dict(self):
+        """
+         returns a dictionary containing all keys/values of __dict__ of the
+         instance
+        """
+        new_dict = dict(self.__dict__)
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        return new_dict
