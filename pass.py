3fd95ab57d019ef11c46a626b1424e04e6149a66
jeffykings <jeffykings@gmail.com>
2025-06-23 12:35:55 +0100

    task1

---
+++ pass.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""A code that summs to numbers
+"""
+
+
+def sum(a, b):
+    """sums to numbers
+
+    Variable:
+        a:  first value
+        b: second value
+
+        Return: the result
+    """
+
+    return (a + b)
+
+
+print("The result is ", sum(1, 2))
