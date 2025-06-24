0ebfefb44d9f86bb17911f10d11467ccf77720d8
jeffykings <jeffykings@gmail.com>
2025-06-24 01:04:50 +0100

    tests_fix

---
+++ tests/test_base_model.py
@@ -0,0 +1,67 @@
+"""Testing the `base_model` module."""
+import time
+import unittest
+import uuid
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """Test cases for the `Base` class.
+    """
+
+    def setUp(self):
+        pass
+
+    def test_initialization_positive(self):
+        """Test passing cases `BaseModel` initialization.
+        """
+        b1 = BaseModel()
+        b2_uuid = str(uuid.uuid4())
+        b2 = BaseModel()
+        b2.id = b2_uuid
+        b2.name="The weeknd"
+        b2.album="Trilogy"
+        self.assertIsInstance(b1.id, str)
+        self.assertIsInstance(b2.id, str)
+        self.assertEqual(b2_uuid, b2.id)
+        self.assertEqual(b2.album, "Trilogy")
+        self.assertEqual(b2.name, "The weeknd")
+        self.assertIsInstance(b1.created_at, datetime)
+        self.assertIsInstance(b1.created_at, datetime)
+        self.assertEqual(str(type(b1)),
+                         "<class 'models.base_model.BaseModel'>")
+
+    def test_dict(self):
+        """Test method for dict"""
+        b1 = BaseModel()
+        b2_uuid = str(uuid.uuid4())
+        b2 = BaseModel()
+        b2.id = b2_uuid
+        b2.name="The weeknd"
+        b2.album="Trilogy"
+        b1_dict = b1.to_dict()
+        self.assertIsInstance(b1_dict, dict)
+        self.assertIn('id', b1_dict.keys())
+        self.assertIn('created_at', b1_dict.keys())
+        self.assertIn('updated_at', b1_dict.keys())
+        self.assertEqual(b1_dict['__class__'], type(b1).__name__)
+
+    def test_save(self):
+        """Test method for save"""
+        b = BaseModel()
+        time.sleep(0.5)
+        date_now = datetime.now()
+        b.save()
+        diff = b.updated_at - date_now
+        self.assertTrue(abs(diff.total_seconds()) < 0.01)
+
+    def test_str(self):
+        """Test method for str representation"""
+        b1 = BaseModel()
+        string = f"[{type(b1).__name__}] ({b1.id}) {b1.__dict__}"
+        self.assertEqual(b1.__str__(), string)
+
+
+if __name__ == "__main__":
+    unittest.main()
