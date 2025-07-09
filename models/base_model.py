c045f3e60530ec57074d8e47b174efa3c0a9dd71
jeffykings <jeffykings@gmail.com>
2025-07-09 09:33:02 +0100

    task5

--- models/base_model.py
+++ models/base_model.py
@@ -25,6 +25,8 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            from models import storage
+            storage.new(self)
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -34,7 +36,10 @@ class BaseModel():
         changene the public instance attribute updated_at with the current
         datetime = self.__dict__[created_at]
         """
+
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
